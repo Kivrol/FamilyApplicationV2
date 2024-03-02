0aef6ebeae58b64a27cee01433b72200fca1ab93
kivrol <vostrikov521@mail.ru>
2024-03-02 14:31:11 +0300

    added profile html and css template

--- myapp/main/urls.py
+++ myapp/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='calendar'),
+    path('profile', views.profileView, name='profile'),
 ]
--- myapp/main/views.py
+++ myapp/main/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def profileView(request):
+    return render(request, 'main/profile.html')
