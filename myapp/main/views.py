ba04f925ddbc18bc3fab6a9ad561aa881500cb32
kivrol <vostrikov521@mail.ru>
2024-03-02 15:28:48 +0300

    added login  html templates

--- myapp/main/views.py
+++ myapp/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'main/index.html')
 
 
+@login_required
 def profileView(request):
     return render(request, 'main/profile.html')
